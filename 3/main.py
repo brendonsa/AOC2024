c5b9f12d187da19899a3910f5309deffaf1bef7e
Brendonas Stakauskas <brendonas.st@gmail.com>
2024-12-03 09:44:15 +0200

    day 3

---
+++ 3/main.py
@@ -0,0 +1,58 @@
+import re
+import numpy as np
+
+# Read input from input.txt
+data = open('input.txt', 'r').read()
+
+# Build regex for finding mul(\d+,\d+)
+
+
+# Part1
+mul_re = re.compile('mul\((\d+),(\d+)\)')
+find = mul_re.findall(data)
+res = 0
+for f in find:
+    l, r = int(f[0]), int(f[1])
+    res += l*r
+print(res)
+# Part2
+
+# Regex for deleting don\'t\(\).*do() sequences
+disable_re = re.compile('don\'t\(\).*?do\(\)')
+data = data.replace('\n', '')
+disable_last = re.compile('don\'t.*')
+data_sub = disable_re.sub('', data)
+data_sub = disable_last.sub('', data_sub)
+find = mul_re.findall(data_sub)
+res = 0
+for f in find:
+    l, r = int(f[0]), int(f[1])
+    res += l*r
+
+print(res)
+
+
+# Eliminate without regex.
+mask = np.ones(len(data), dtype=bool)
+curr = True
+for idx in range(len(data)):
+    dont = data[idx:idx+7]
+    do = data[idx:idx+4]
+    if dont == 'don\'t()':
+        curr = False
+    elif do == 'do()':
+        curr = True
+    mask[idx] = curr
+data_np = np.array(list(data))
+data_sub = data_np[mask]
+data_sub = ''.join(data_sub)
+
+
+find = mul_re.findall(data_sub)
+res = 0
+for f in find:
+    l, r = int(f[0]), int(f[1])
+    res += l*r
+
+
+print(res)
