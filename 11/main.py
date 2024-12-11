93f3cecdfcb7be4524761d16713e942de245fbff
Brendonas Stakauskas <brendonas.st@gmail.com>
2024-12-11 10:05:45 +0200

    add cache solution

--- 11/main.py
+++ 11/main.py
@@ -1,34 +1,53 @@
 import tqdm
-from functools import cache
+from functools import lru_cache
 
 data = open('input.txt', 'r').read()
 data = data.split(' ')
+data = list(map(int, data))
 
-# @cache
+
+@lru_cache(maxsize=None)
 def change_stone(s):
-    if s == '0':
-        return ['1']
-    elif len(s)%2==0:
-        part = len(s)//2
-        return [s[:part], str(int(s[part:]))]
+    if s == 0:
+        return [1]
+    elif len(str(s)) % 2 == 0:
+        part = len(str(s))//2
+        return [int(str(s)[:part]), int(str(s)[part:])]
     else:
-        return [str(int(s)*2024)]
+        return [s*2024]
 
 
 def blink(stones):
     stones_new = []
     for s in stones:
-        stones_new +=change_stone(s)
+        stones_new += change_stone(s)
     return stones_new
+
+
+@lru_cache(maxsize=None)
+def blink_r(stone, depth):
+    count = 0
+    if depth == 1:
+        s = change_stone(stone)
+        count += len(s)
+        return count
+    else:
+        stones = change_stone(stone)
+        for s in stones:
+            count += blink_r(s, depth-1)
+    return count
+
 
 stones = data
 
-for i in range(25):
-    stones = blink(stones)
+count = 0
+for s in stones:
+    count += blink_r(s, 25)
 
-print(len(stones))
+print(count)
 
-for i in tqdm.tqdm(range(50)):
-    stones = blink(stones)
-    
+count = 0
+for s in stones:
+    count += blink_r(s, 75)
 
+print(count)
